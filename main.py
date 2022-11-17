4ed30a6778a0ec7e40d3608ec86b1c06d7f177a9
nataliacatundafr <ncatundafr@gmail.com>
2022-11-17 13:52:15 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def Max(primeiro, segundo):
+    if primeiro>segundo:
+        return A
+    if primeiro<segundo:
+        return B
+
+
+
+
+print("Este programa vai pedir pra voce entrar com 2 números inteiros")
+A = int( input("Digite o primeiro "))
+B = int( input("Digite o segundo "))
+print("O maior número é:", Max(A,B))
+
+
