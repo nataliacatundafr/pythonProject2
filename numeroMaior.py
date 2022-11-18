66e3ba4899c3f0d4f0404817acfe687463d9542c
nataliacatundafr <ncatundafr@gmail.com>
2022-11-18 09:51:58 -0300

    second commit

--- numeroMaior.py
+++ numeroMaior.py
@@ -1,17 +1,14 @@
 def Max(primeiro, segundo):
-    if A>B and B<A: return segundo
-    if B>A and  B<A:return primeiro
-    if C>D and C>A and C>B :return segundo
-    if D>C and D>A and D>B : return primeiro
-
+    if primeiro>segundo: return primeiro
+    else: return segundo
 print("Este programa vai pedir pra voce entrar com 2 números inteiros")
 A = int( input("Digite o primeiro "))
 B = int( input("Digite o segundo "))
-Max(A,B)
-print("O maior número é:",     Max(A,B)    );
+X = Max(A,B)
+print("O maior número é:",     X   );
 print("Mais uma vez!")
 C = int( input("Digite o primeiro "))
 D = int( input("Digite o segundo "))
-Max(C,D)
-print("O maior número é:",   Max(C,D) );
-print("Dentre os 4 numeros q vc digitou",  Max(C,D)   , "é o maior!");
+Y = Max(C,D)
+print("O maior número é:",    Y  );
+print("Dentre os 4 numeros q vc digitou",    Max(X,Y)    , "é o maior!");
